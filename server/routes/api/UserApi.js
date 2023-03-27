26ca0813c51026dc23c1eb6aebae55c6235b9491
trieunnh5903 <111626907+trieunnh5903@users.noreply.github.com>
2023-03-27 08:45:20 +0700

    Update UserApi.js

--- server/routes/api/UserApi.js
+++ server/routes/api/UserApi.js
@@ -3,12 +3,18 @@ const router = express.Router();
 const userController = require('../../components/users/UserController');
 //localhost:3000/api/user/login
 router.post('/login', async function (req, res, next) {
-    const { email, password } = req.body;
-    const result = await userController.login(email, password);
-    if (result) {
-       return res.json({error: false, email: email, password: password});
-    } else {
-        return res.json({error: true, message: 'Email or password failed'});
+    try {
+        const { email, password } = req.body;
+        const result = await userController.login(email, password);
+        if (result) {
+           return res.status(200).json({error: false, email: email, password: password});
+        } else {
+            return res.status(400).json({error: true, message: 'Email or password failed'});
+        }  
+    } catch (error) {
+        return res.status(400).json({error: true, message: error});
+        
     }
+   
 });
 module.exports = router;
