930ab2b07a504f66107c41d7b040c5d232ed2a8a
trieunnh5903 <111626907+trieunnh5903@users.noreply.github.com>
2023-09-28 09:40:10 +0700

    .

---
+++ routes/api/ProductApi.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const productController = require("../../components/products/ProductController");
+
+// http://localhost:3000/api/product
+router.get("/", async function (req, res, next) {
+  try {
+    const { idCategory, id } = req.query;
+    if (idCategory) {
+      const products = await productController.getProductByCategory(idCategory);
+      return res.status(200).json(products);
+    }
+
+    if (id) {
+      const products = await productController.getProductBuyId(id);
+      return res.status(200).json(products);
+    }
+  } catch (error) {
+    res.status(400).json({});
+  }
+});
+
+router.get("/search", async (req, res, next) => {
+  try {
+    const { q } = req.query;
+    const product = await productController.search(q);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(400).json({});
+  }
+});
+
+module.exports = router;
